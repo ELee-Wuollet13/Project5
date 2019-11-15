2d8d92cb4663b0d4617a033d8a889d499c4dc3d5
Student <example@example.com>
2019-11-15 15:26:03 -0800

    new idea for js using for and if else

--- src/back.js
+++ src/back.js
@@ -1,48 +1,56 @@
-export { earthCalculator }
-export { mercCalculator }
-export { venusCalculator }
-export { marsCalculator }
-export { jupitorCalculator }
-export { saturnCalculator }
-export { uranusCalculator }
-export { neptuneCalculator }
-export { plutoCalculator }
+export { solarAge }
+
+const planets = [];
 
 
-const earthCalculator = function(age) {
-  let earthAge = 0;
-  return (earthAge = age)
-}
-const mercCalculator = function(age) {
-  let mercAge = 0;
-  return (mercAge = age / .24)
-}
-const venusCalculator = function(age) {
-  let venusAge = 0;
-  return (venusAge = age / .615).toFixed(0);
-}
-const marsCalculator = function(age) {
-  let marsAge = 0;
-  return (marsAge = age / 1.8).toFixed(0);
-}
-const jupitorCalculator = function(age) {
-  let jupitorAge = 0;
-  return (jupitorAge = age / 11.86).toFixed(1);
-}
 
-const saturnCalculator = function(age) {
-  let satrunAge = 0;
-  return (satrunAge = age / 29.46).toFixed(2)
-}
-const uranusCalculator = function(age) {
-  let uranusAge = 0;
-  return (uranusAge = age / 84.01).toFixed(2);
-}
-const neptuneCalculator = function(age) {
-  let neptuneAge = 0;
-  return (neptuneAge = age / 164.8).toFixed(2);
-}
-const plutoCalculator = function(age) {
-  let plutoAge = 0;
-  return (plutoAge = age / 248.6).toFixed(2);
-}
+
+
+// export { earthCalculator }
+// export { mercCalculator }
+// export { venusCalculator }
+// export { marsCalculator }
+// export { jupitorCalculator }
+// export { saturnCalculator }
+// export { uranusCalculator }
+// export { neptuneCalculator }
+// export { plutoCalculator }
+//
+//
+// const earthCalculator = function(age) {
+//   let earthAge = 0;
+//   return (earthAge = age)
+// }
+// const mercCalculator = function(age) {
+//   let mercAge = 0;
+//   return (mercAge = age / .24)
+// }
+// const venusCalculator = function(age) {
+//   let venusAge = 0;
+//   return (venusAge = age / .615).toFixed(0);
+// }
+// const marsCalculator = function(age) {
+//   let marsAge = 0;
+//   return (marsAge = age / 1.8).toFixed(0);
+// }
+// const jupitorCalculator = function(age) {
+//   let jupitorAge = 0;
+//   return (jupitorAge = age / 11.86).toFixed(1);
+// }
+//
+// const saturnCalculator = function(age) {
+//   let satrunAge = 0;
+//   return (satrunAge = age / 29.46).toFixed(2)
+// }
+// const uranusCalculator = function(age) {
+//   let uranusAge = 0;
+//   return (uranusAge = age / 84.01).toFixed(2);
+// }
+// const neptuneCalculator = function(age) {
+//   let neptuneAge = 0;
+//   return (neptuneAge = age / 164.8).toFixed(2);
+// }
+// const plutoCalculator = function(age) {
+//   let plutoAge = 0;
+//   return (plutoAge = age / 248.6).toFixed(2);
+// }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import $ from 'jquery';
+// import 'solarAge';
 
 $(document).ready(function() {
   $("form#intake").submit(function(event) {
     let age = ("#age").val();
     console.log(age);
-    
+
     event.preventDefault()
   })
 })
