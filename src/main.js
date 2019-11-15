eb8a673ce46d68994a8f93dee44c8a8793220c8e
Student <example@example.com>
2019-11-15 15:06:30 -0800

    still working -building some frontend UI and researching classes to dry up back.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import $ from 'jquery';
+
+$(document).ready(function() {
+  $("form#intake").submit(function(event) {
+    let age = ("#age").val();
+    console.log(age);
+    
+    event.preventDefault()
+  })
+})
