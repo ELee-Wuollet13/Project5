23a082406be223ec601ab7cd8e3de394da88efc1
Student <example@example.com>
2019-11-27 14:21:53 -0800

    project fails as intended

--- __test__/project5.test.js
+++ __test__/project5.test.js
@@ -3,10 +3,10 @@ import { SolarAge } from "../src/back.js";
 
 describe('SolarAge', () => {
   test('should take in the users age and return their age on Earth', () => {
-    let age = 30;
+    let age = 3;
     let planet = "earth"
     let earthAge = new SolarAge (age, planet);
-    expect(earthAge).toEqual(30);
+    expect(earthAge).toEqual({"age": 30, "localAge": 30, "planet": "earth"});
   });
   // test('should take in the users age and return their age on Mercury', () => {
   //   let age = 30;
